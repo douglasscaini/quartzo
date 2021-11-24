267c5368d137a6af71a1676f924aaee303bc0a86
Douglas Scaini <douglasscaini.dev@gmail.com>
2021-11-24 10:28:56 -0300

    fix: Rename Views Pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,10 @@ import { VStack } from "@chakra-ui/react";
 import Head from "next/head";
 
 import { Header } from "../components/Header";
-import { HomePage } from "../app/pages/HomePage";
-import { About } from "../app/pages/About";
-import { Gallery } from "../app/pages/Gallery";
-import { Contact } from "../app/pages/Contact";
+import { HomePage } from "../app/views/HomePage";
+import { About } from "../app/views/About";
+import { Gallery } from "../app/views/Gallery";
+import { Contact } from "../app/views/Contact";
 
 export default function Home() {
   return (
